f5962642d51f549943629c5ff62caff4b9314cab
mariemkahli1 <kahli.mariem@enis.tn>
2025-01-16 22:30:44 +0100

    Update login.component.ts

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -7,28 +7,25 @@ import { Router } from '@angular/router';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
-  private apiUrl = 'http://localhost:5146/api/Authentification'; // URL de l'API pour l'authentification
+ user: UtilisateurDto = { NomUtilisateur: '', MotPasse: '' };
+  errorMessage: string = '';
 
-  constructor(private http: HttpClient, private router: Router) {}
+  constructor(private authService: AuthService, private router: Router) {}
 
-  // Méthode pour se connecter
-  login(user: UtilisateurDto): Observable<string> {
-    return this.http.post<string>(this.apiUrl, user);
-  }
-
-  // Méthode pour vérifier si l'utilisateur est connecté
-  isAuthenticated(): boolean {
-    return !!localStorage.getItem('token');
-  }
-
-
-  // Méthode pour obtenir le rôle de l'utilisateur à partir du token
-  getRole(): string | null {
-    const token = this.getToken();
-    if (token) {
-      const decodedToken = JSON.parse(atob(token.split('.')[1])); // Décoder le JWT pour obtenir les claims
-      return decodedToken.role;
-    }
-    return null;
+  login() {
+    this.authService.login(this.user).subscribe({
+      next: (token) => {
+        localStorage.setItem('token', token);
+        const role = this.authService.getRole();
+        if (role === 'Responsable') {
+          this.router.navigate(['/dashboard']); // Redirection vers la page du responsable
+        } else if (role === 'Client') {
+          this.router.navigate(['/reclamations']); // Redirection vers la page du client
+        }
+      },
+      error: () => {
+        this.errorMessage = 'Nom d\'utilisateur ou mot de passe incorrect';
+      }
+    });
   }
 }
